584bb860b17ccda9fcc45f6ad46f70b8994fcfe6
johnlk <jlkuhn@purdue.edu>
2018-01-19 19:44:46 -0500

    updated background color, started functionality for cloud spawning

--- eatingClouds/eatingClouds/GameScene.swift
+++ eatingClouds/eatingClouds/GameScene.swift
@@ -17,18 +17,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsWorld.contactDelegate = self
         
         //setting background color
-        let backgroundColor = SKColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1)
+        let backgroundColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 221.0/255.0, alpha: 1.0)
         self.backgroundColor = backgroundColor
         
-        //draw a cloud
-        let cloudTexture = SKTexture(imageNamed: "./clouds/cloud1.png")
-        cloudTexture.filteringMode = .nearest
+        //cloud textures
+        let cloudTexture1 = SKTexture(imageNamed: "./clouds/cloud1.png")
+        cloudTexture1.filteringMode = .nearest
+        let cloudTexture2 = SKTexture(imageNamed: "./clouds/cloud2.png")
+        cloudTexture2.filteringMode = .nearest
+        let cloudTexture3 = SKTexture(imageNamed: "./clouds/cloud3.png")
+        cloudTexture3.filteringMode = .nearest
         
-        let cloud = SKSpriteNode(texture: cloudTexture)
-        cloud.position = CGPoint(x: 0.0, y: 0.0)
+        //cloud movement
+        let distanceToMove = CGFloat(self.frame.width * cloudTexture1.size().width)
+        let cloudMoves = SKAction.moveBy(x: -distanceToMove, y: 0.0, duration: TimeInterval(0.01 * distanceToMove))
+        let removeCloud = SKAction.removeFromParent()
+        let makeCloudsAndRemove = SKAction.sequence([cloudMoves, removeCloud])
+        
+        //spawn clouds
+//        let spawn = SKAction.run(makeCloudsAndRemove)
+        let delay = SKAction.wait(forDuration: TimeInterval(2.0))
+        
+        let cloud = SKSpriteNode(texture: cloudTexture1)
+        cloud.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.5)
         
         self.addChild(cloud)
         
+        
+    }
+    
+    func makeCloudsAndRemove(){
         
     }
 
