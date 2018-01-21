826cb80855c9441a687a89b5a1178471a8e39da1
johnlk <jlkuhn@purdue.edu>
2018-01-20 23:49:44 -0500

    gave spidy some physics and he responds to touches

--- eatingClouds/eatingClouds/GameScene.swift
+++ eatingClouds/eatingClouds/GameScene.swift
@@ -13,15 +13,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let personCategory: UInt32 = 1 << 0
     let cloudCategory: UInt32 = 1 << 1
     
+    var clouds: SKNode!
+    
     var cloudTexture1: SKTexture!
     var cloudTexture2: SKTexture!
     var cloudTexture3: SKTexture!
     
     var moveCloudAndRemove: SKAction!
     
+    var spiderman: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         //physics
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.0)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -5.0)
         self.physicsWorld.contactDelegate = self
         
         //setting background color
@@ -45,23 +49,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         cityBackground.position = CGPoint(x: 0.0, y: 0.0)
         cityBackground.zPosition = -1
         self.addChild(cityBackground)
+        
         //spiderman
         //let and var
-        let textureOne = SKTexture(imageNamed:"./pics/standing.png")//1 param
-        let textureTwo=SKTexture(imageNamed:"./pics/sideshot.png")
+        let textureOne = SKTexture(imageNamed: "./pics/standing.png")//1 param
+        let textureTwo = SKTexture(imageNamed: "./pics/sideshot.png")
         textureOne.filteringMode = .nearest
         textureTwo.filteringMode = .nearest
         
-        let animation=SKAction.animate(with:[textureOne,textureTwo],timePerFrame:0.2)
-        let walking=SKAction.repeatForever(animation)
-        let spiderman = SKSpriteNode(texture:textureOne)
+        let animation = SKAction.animate(with: [textureOne, textureTwo], timePerFrame:0.2)
+        let walking = SKAction.repeatForever(animation)
+        spiderman = SKSpriteNode(texture: textureOne)
         spiderman.setScale(0.8)
-        spiderman.position=CGPoint(x:self.frame.width*0.2,y:self.frame.height*0.3)
+        spiderman.position=CGPoint(x: self.frame.width * 0.2, y: self.frame.height * 0.3)
         
         spiderman.run(walking)
+        
+        spiderman.physicsBody = SKPhysicsBody(rectangleOf: spiderman.size)
+        spiderman.physicsBody?.isDynamic = true
+        spiderman.physicsBody?.allowsRotation = false
+        
+        spiderman.physicsBody?.categoryBitMask = personCategory
+        spiderman.physicsBody?.collisionBitMask = cloudCategory
+        spiderman.physicsBody?.contactTestBitMask = cloudCategory
+        
         self.addChild(spiderman)
+        
 
         //cloud textures
+        clouds = SKNode()
+        self.addChild(clouds)
+        
         cloudTexture1 = SKTexture(imageNamed: "./clouds/cloud1.png")
         cloudTexture1.filteringMode = .nearest
         cloudTexture2 = SKTexture(imageNamed: "./clouds/cloud2.png")
@@ -71,7 +89,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //cloud movement
         let distanceToMove = CGFloat(self.frame.width * cloudTexture1.size().width)
-        let cloudMoves = SKAction.moveBy(x: -distanceToMove, y: 0.0, duration: TimeInterval(0.01 * distanceToMove))
+        let cloudMoves = SKAction.moveBy(x: -distanceToMove * 0.5, y: 0.0, duration: TimeInterval(0.01 * distanceToMove))
         let removeCloud = SKAction.removeFromParent()
         moveCloudAndRemove = SKAction.sequence([cloudMoves, removeCloud])
         
@@ -85,22 +103,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func makeCloudsAndRemove(){
+        let cloudNode = SKNode()
+        
         let cloud = SKSpriteNode(texture: cloudTexture1)
         cloud.setScale(2.5)
-        cloud.position = CGPoint(x: self.frame.width + (cloud.size.width * 0.3),
-                                 y: self.frame.height * CGFloat(arc4random_uniform(5))/10 + cloud.size.height)
         
-        cloud.physicsBody = SKPhysicsBody(rectangleOf: cloud.size)
-        cloud.physicsBody?.isDynamic = false
-        cloud.physicsBody?.categoryBitMask = cloudCategory
-        cloud.physicsBody?.contactTestBitMask = personCategory
+        cloudNode.position = CGPoint(x: self.frame.width + (cloud.size.width * 0.3),
+                                    y: self.frame.height * CGFloat(arc4random_uniform(5))/10 + cloud.size.height)
         
-        cloud.run(moveCloudAndRemove)
-        self.addChild(cloud)
+        cloudNode.addChild(cloud)
+        
+        cloudNode.physicsBody = SKPhysicsBody(rectangleOf: cloud.size)
+        cloudNode.physicsBody?.isDynamic = false
+        cloudNode.physicsBody?.categoryBitMask = cloudCategory
+        cloudNode.physicsBody?.contactTestBitMask = personCategory
+        
+        cloudNode.run(moveCloudAndRemove)
+        clouds.addChild(cloudNode)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for _ in touches {
+            spiderman.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
+            spiderman.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: self.frame.height * 0.25))
+        }
     }
     
     
